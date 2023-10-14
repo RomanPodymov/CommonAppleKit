35519bc7fae5cf1e615228dcd431ff3917fbe232
Roman Podymov <podymfrombryansk@yandex.com>
2023-10-15 00:48:16 +0200

    Tests

---
+++ Tests/CommonAppleKit/CommonAppleKitTests.swift
@@ -0,0 +1,16 @@
+//
+//  CommonAppleKitTests.swift
+//  CommonAppleKitTests
+//
+//  Created by Roman Podymov on 15/10/2023.
+//  Copyright © 2023 CommonAppleKit. All rights reserved.
+//
+
+import XCTest
+@testable import CommonAppleKit
+
+final class CommonAppleKitTests: XCTestCase {
+    func test() {
+        
+    }
+}
