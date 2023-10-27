6a3fd30ae92bd0072dc9e29320f1e3916cc6c8d4
Roman Podymov <podymfrombryansk@yandex.com>
2023-10-27 12:31:01 +0200

    CAAppDelegate

---
+++ Sources/CommonAppleKit/CAAppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  CAAppDelegate.swift
+//  CommonAppleKit
+//
+//  Created by Roman Podymov on 27/10/2023.
+//  Copyright © 2023 CommonAppleKit. All rights reserved.
+//
+
+import Foundation
+
+open class CAAppDelegate: CAAppDelegateBaseClass, CAApplicationDelegate {
+    open var window: CAWindow!
+}
